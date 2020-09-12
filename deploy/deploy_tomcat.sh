5a6a237a23360e60b2d337f03c29be173cbdbde6
bfrlbfq1 <49507013+bfrlbfq1@users.noreply.github.com>
2020-09-12 11:49:23 +0800

    Update deploy_tomcat.sh

--- deploy/deploy_tomcat.sh
+++ deploy/deploy_tomcat.sh
@@ -11,7 +11,7 @@
 export mysql_ip=129.211.167.92
 export mysql_port=3306
 export mysql_user=root
-export mysql_pwd=123456
+export mysql_pwd=1qaz2wsx
 export TOMCAT_PATH=/usr/local/apache-tomcat-8.5.57
 export PROJ_PATH=`pwd`
 
